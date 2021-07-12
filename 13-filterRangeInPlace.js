b4bf3a751e56d2481504fbcead15bbf38af50f5d
Ihor RSS <superfirfox@gmail.com>
2021-07-12 12:58:04 +0300

    add 13 in type-basic

--- 13-filterRangeInPlace.js
+++ 13-filterRangeInPlace.js
@@ -1,5 +1,12 @@
 "use strict"
 
+let arr = [5, 3, 8, 1];
+
 function filterRangeInPlace(arr, a, b){
-    
+    arr.map((item, index, arr) => 
+        (item<a || item>b) ? arr.splice(index, 1) : null);
 }
+
+filterRangeInPlace(arr, 1, 4);
+
+console.log(arr);
